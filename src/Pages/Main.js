6fed36594d68dd065ddbbe636b7f57f9d8b33619
chajinjoo <chajanee@gmail.com>
2019-12-24 14:56:12 +0900

    wip

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "../Pages/Main.scss";
+import Nav from "../components/Nav/Nav";
+import MainHeader from "../components/MainHeader/MainHeader";
+import Manuicon from "../components/Menuicon/Menuicon";
 
 function Main() {
   return (
     <div>
-      <h1>하하하하하하하 우리는 위홈</h1>
+      <Nav />
+      <MainHeader />
+      <Manuicon />
+      {/* <h1>하하하하하하하 우리는 위홈</h1> */}
     </div>
   );
 }
