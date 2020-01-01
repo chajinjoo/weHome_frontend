2483f29b723aed6ef10a0cfff01c56b951ab6612
chajinjoo <chajanee@gmail.com>
2020-01-01 19:22:08 +0900

    반응형 수정

--- src/Component/Nav/Nav.js
+++ src/Component/Nav/Nav.js
@@ -12,62 +12,78 @@ class Nav extends Component {
           href="https://use.fontawesome.com/releases/v5.9.0/css/all.css"
         />
         <div className="nav">
-          <div className="nav_left">
-            <img className="home_logo" src={home_logo} alt="home_logo" />
-            <ul className="nav_lists">
-              <li className="nav_list">커뮤니티</li>
-              <li id="list1" className="nav_list">
-                스토어
-              </li>
-              <li className="nav_list">전문가시공</li>
-            </ul>
-          </div>
-
-          <div className="nav_center">
-            <i className="fas fa-search"></i>
-            <input className="search" placeholder="오늘의집 통합검색" />
-            <button className="nav_btn">글쓰기</button>
-          </div>
-
-          <div className="nav_right">
-            <div className="nav_icon_box">
-              <i className="far fa-bookmark"></i>
-            </div>
-            <div className="nav_icon_box">
-              <i className="far fa-bell"></i>
-            </div>
-            <div className="nav_icon_box">
-              <i className="fas fa-shopping-cart"></i>
-            </div>
-            <button className="nav_icon_btn">
-              <div className="nav_icon_box">
-                <img
-                  className="smile"
-                  src="https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-default_images-avatar.png/36/36"
-                  alt="smile"
-                />
+          <div className="nav_body">
+            <div className="nav_left">
+              {/* <div></div> */}
+              <img
+                id="nav_main_logo"
+                className="home_logo"
+                src={home_logo}
+                alt="home_logo"
+              />
+              <div className="nav_left_btns">
+                <ul className="nav_lists">
+                  <li className="nav_list">커뮤니티</li>
+                  <li id="list1" className="nav_list">
+                    스토어
+                  </li>
+                  <li className="nav_list">전문가시공</li>
+                </ul>
               </div>
-              <i className="fas fa-caret-down"></i>
-            </button>
+            </div>
+
+            <div className="nav_right_container">
+              <div className="nav_center">
+                <i className="fas fa-search"></i>
+                <input className="search" placeholder="오늘의집 통합검색" />
+                <button className="nav_btn">글쓰기</button>
+              </div>
+
+              <div className="nav_right">
+                <div className="nav_right_icon_box">
+                  <div className="nav_icon_box">
+                    <i className="far fa-bookmark"></i>
+                  </div>
+                  <div className="nav_icon_box">
+                    <i className="far fa-bell"></i>
+                  </div>
+                  <div className="nav_icon_box">
+                    <i className="fas fa-shopping-cart"></i>
+                  </div>
+                </div>
+                <button className="nav_icon_btn">
+                  <div className="nav_icon_box">
+                    <img
+                      className="smile"
+                      src="https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-default_images-avatar.png/36/36"
+                      alt="smile"
+                    />
+                  </div>
+                  <i className="fas fa-caret-down"></i>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
 
         <div className="category">
-          <div className="category_left">
-            <ul className="category_lists">
-              <li className="category_list">홈</li>
-              <li className="category_list">사진</li>
-              <li className="category_list">집들이</li>
-              <li className="category_list">노하우</li>
-              <li className="category_list">전문가집들이</li>
-              <li className="category_list">셀프가이드</li>
-              <li className="category_list">질문과답변</li>
-              <li className="category_list">이벤트</li>
-            </ul>
-          </div>
-          <div className="category_right">
-            <img className="house_logo" src={house_logo} alt="house_logo" />
-            <span className="download">앱다운로드</span>
+          <div className="category_body">
+            <div className="category_left">
+              <ul className="category_lists">
+                <li className="category_list">홈</li>
+                <li className="category_list">사진</li>
+                <li className="category_list">집들이</li>
+                <li className="category_list">노하우</li>
+                <li className="category_list">전문가집들이</li>
+                <li className="category_list">셀프가이드</li>
+                <li className="category_list">질문과답변</li>
+                <li className="category_list">이벤트</li>
+              </ul>
+            </div>
+            <div className="category_right">
+              <img className="house_logo" src={house_logo} alt="house_logo" />
+              <span className="download">앱다운로드</span>
+            </div>
           </div>
         </div>
       </>
